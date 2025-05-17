b38bfd42d87bbb74c77730faa0aa39406dfc5a1f
HazaFG <hazaelfg@gmail.com>
2025-05-17 16:03:50 -0600

    a ver si no borre src

--- src/actores/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ActoresPage() {
-    return (
-        <>
-        <div className = "">Hola perrillo</div>
-        vamos a meter este error nomas, por favor foto ya muestrate xd
-        </>
-    )
-}
