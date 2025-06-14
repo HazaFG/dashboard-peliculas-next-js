16eea78179839d893d1fba0308b26b74947820f8
HazaFG <hazaelfg@gmail.com>
2025-06-14 15:51:23 -0700

    mira, ves como si entendiste como pasar el arreglo a la card

--- src/peliculas/components/CreditsCard.tsx
+++ src/peliculas/components/CreditsCard.tsx
@@ -1,6 +1,26 @@
-export const CreditsCard = () => {
+import Image from 'next/image'
+import { ActorPelicula } from '../interfaces/pelicula-castmember'
+
+interface Props {
+  actoresArray: ActorPelicula;
+}
+
+export const CreditsCard = ({ actoresArray }: Props) => {
   return (
-    <div>Esto sera el futuro card</div>
+    <>
+      <div key={actoresArray.id} className="flex-shrink-0 w-32 text-center">
+        <div className="relative overflow-hidden bg-gray-300 w-32 h-32 rounded-full mx-auto mb-2 flex items-center justify-center text-gray-500 text-sm shadow-md">
+          <Image
+            src={`https://image.tmdb.org/t/p/w200${actoresArray.profile_path}`}
+            fill
+            alt={`Foto de ${actoresArray.name}`}
+            className="object-cover"
+          />
+        </div>
+        <p className="font-semibold text-sm text-gray-800">{actoresArray.name}</p>
+        <p className="text-gray-600 text-xs">{actoresArray.character}</p>
+      </div>
+    </>
   )
 }
 
--- src/peliculas/components/CreditsGrid.tsx
+++ src/peliculas/components/CreditsGrid.tsx
@@ -1,5 +1,5 @@
 import { ActorPelicula } from "../interfaces/pelicula-castmember"
-import Image from 'next/image'
+import { CreditsCard } from "./CreditsCard"
 
 interface Props {
   actoresArray: ActorPelicula[]
@@ -12,18 +12,7 @@ export const CreditsGrid = ({ actoresArray }: Props) => {
         <h2 className="text-2xl font-bold mb-6 text-gray-800">Actores Principales</h2>
         <div className="flex flex-wrap justify-center gap-6 pb-4">
           {actoresArray.map(actor => (
-            <div key={actor.id} className="flex-shrink-0 w-32 text-center">
-              <div className="relative overflow-hidden bg-gray-300 w-32 h-32 rounded-full mx-auto mb-2 flex items-center justify-center text-gray-500 text-sm shadow-md">
-                <Image
-                  src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
-                  fill
-                  alt={`Foto de ${actor.name}`}
-                  className="object-cover"
-                />
-              </div>
-              <p className="font-semibold text-sm text-gray-800">{actor.name}</p>
-              <p className="text-gray-600 text-xs">{actor.character}</p>
-            </div>
+            <CreditsCard actoresArray={actor} key={actor.id} />
           ))}
         </div>
       </div>
